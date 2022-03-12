9068a59d2a6bb5b6a2f4386d3ec1633a028c4fa1
hugosenari <hugosenari@gmail.com>
2022-03-12 02:23:35 -0300

    chore: re index flickr_image_bot

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Twitter bot for fetching flickr images with tags'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."flickr_image_bot-master".url = "path:./master";
+  inputs."flickr_image_bot-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."flickr_image_bot-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
